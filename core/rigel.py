a812fe4a51192160f1d0b2eb20e6ec575d5c783c
Zerone Brianstorm <ometha12@gmail.com>
2025-06-15 08:18:13 +0530

    feat: added groq support

--- core/rigel.py
+++ core/rigel.py
@@ -1,7 +1,10 @@
-from langchain_ollama import ChatOllama
 from langchain_core.messages import AIMessage
 from langchain_core.prompts import ChatPromptTemplate
 from logger import SysLog
+import os
+import getpass
+from langchain_ollama import ChatOllama
+from langchain_groq import ChatGroq
 syslog = SysLog(name="RigelEngine", level="DEBUG", log_file="rigel.log")
 hello_string = "Zerone Laboratories Systems - RIGEL Engine v4.0[Alpha]\n"
 
@@ -44,9 +47,13 @@ class RigelOllama(Rigel): # RIGEL with ollama backend
         return super().inference(messages)
 
 class RigelGroq(Rigel): # RIGEL with groq backend
-    def __init__(self, model_name: str = "llama3.2"):
+    def __init__(self, model_name: str = "llama3-70b-8192", temp: float = 0.7):
         super().__init__(model_name=model_name, chatmode="groq")
-        # self.llm = ChatGroq(model=self.model)
+        if "GROQ_API_KEY" not in os.environ:
+            os.environ["GROQ_API_KEY"] = getpass.getpass("Enter your Groq API key: ")
+        self.llm = ChatGroq(model=self.model,
+                            temperature=temp,
+                            )
 
     def inference(self, messages: list, model: str = None):
         if model:
@@ -65,3 +72,14 @@ if __name__ == "__main__":
     syslog.debug(f"Example Inference :{messages}")
     response = rigel.inference(messages=messages)
     syslog.debug(response.content)
+
+    # Online Groq inference
+    rigel_groq = RigelGroq(model_name="llama3-70b-8192")
+    syslog.info("Started Rigel Groq with model: {}".format(rigel_groq.model))
+    messages_groq = [
+        ("system", "You are RIGEL, a helpful assistant"),
+        ("human", "Say Hello Groq, Let's get the party started!"),
+    ]
+    syslog.debug(f"Example Inference Groq :{messages_groq}")
+    response_groq = rigel_groq.inference(messages=messages_groq)
+    syslog.debug(response_groq.content)
