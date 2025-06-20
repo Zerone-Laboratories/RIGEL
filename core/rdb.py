143056803135936504ad19b2af00352dcaf9e0d6
Zerone Brianstorm <ometha12@gmail.com>
2025-06-20 22:05:45 +0530

    chore: licensing

--- core/rdb.py
+++ core/rdb.py
@@ -1,11 +1,11 @@
-# This file is part of RIGEL_ENGINE.
+# This file is part of RIGEL Engine.
 #
-# RIGEL_ENGINE is free software: you can redistribute it and/or modify
+# RIGEL Engine is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# RIGEL_ENGINE is distributed in the hope that it will be useful,
+# RIGEL Engine is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Affero General Public License for more details.
